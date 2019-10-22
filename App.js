87c17b8447b43ab27db4f5620a8f1e799ce22128
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-10-22 19:35:15 +0100

    basic function and printing

--- App.js
+++ App.js
@@ -1,3 +1,23 @@
-console.log('Hello world!');
-console.error("Ooooops something went wrong..");
-console.dir({name: 'Michal', age:33});
+// console.log('Hello world!');
+// console.error("Ooooops something went wrong..");
+// console.dir({name: 'Michal', age:33});
+
+
+function printMessage(username, badgeCount, points) {
+   const message =  `${username} has ${badgeCount} total ` +
+                    `badge(s) and ${points} points in JavaScript`;
+   console.log(message)
+}
+
+printMessage(
+    "Michal",
+    999,
+    10000);
+
+//Connect to the API URL
+
+//Read the data
+
+//Parse the data
+
+//Print the data
