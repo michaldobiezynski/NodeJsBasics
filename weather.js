7487f17bec41d970d8a8b9143a80f752db8b3940
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-10-23 20:21:20 +0100

    changed the query sent to the api url so that it returns data in metric units

--- weather.js
+++ weather.js
@@ -3,12 +3,12 @@ const weatherApiKey = "fbcfd7e05f7edae93096af50733bf6ec";
 const https = require('https');
 
 function printWeather(weather) {
-    const message = `Current temperature in ${weather.name} is ${weather.main.temp} F`;
+    const message = `Current temperature in ${weather.name} is ${weather.main.temp} C`;
     console.log(message);
 }
 
 function getBodyFromWeatherApp(city, countryCode) {
-    const request = https.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${weatherApiKey}`,
+    const request = https.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${weatherApiKey}&units=metric`,
         response => {
             let body = "";
 
